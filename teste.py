268bd42a8b802d0a189138353f065ad3c397f1c5
Nilo Marchiori <nilo.marchiori@gmail.com>
2025-02-15 16:36:52 -0300

    Adiconando tempo a tabela de parametros

--- teste.py
+++ teste.py
@@ -1,12 +1,1 @@
 
-import pandas as pd
-
-# Criando um DataFrame com uma célula contendo múltiplas linhas
-data = {
-    "Coluna1": ["Linha1\nLinha2\nLinha3"],
-    "Coluna2": ["Outro valor"]
-}
-
-df = pd.DataFrame(data)
-print(df.to_string(index=False))
-df.to_csv(f/out.csv", index=False)
